961906f0f514739a63b7d164fadbc051a3a58193
temitopeakin1 <temitopeakinmegha@gmail.com>
2025-03-23 16:24:13 +0100

    commit

--- app/index.tsx
+++ app/index.tsx
@@ -24,7 +24,7 @@ const Index = () => {
     data: trendingMovies,
     loading: trendingLoading,
     error: trendingError,
-  } = useFetch(getTrendingMovies);
+  } = seFetch(getTrendingMovies);
 
   const {
     data: movies,
